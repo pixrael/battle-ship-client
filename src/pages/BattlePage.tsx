9c0d27c3fe46ed17e065bfdfef0fa493954e4a25
set.ortiz-monasterio <set.ortiz-monasterio@mongodb.com>
2021-06-21 22:50:46 -0300

    send msg when user clicks a cell

--- src/pages/BattlePage.tsx
+++ src/pages/BattlePage.tsx
@@ -8,7 +8,10 @@ import CardContent from '@material-ui/core/CardContent';
 import io from 'socket.io-client';
 import { Button } from '@material-ui/core';
 import WavesIcon from '@material-ui/icons/Waves';
+import BlurOnIcon from '@material-ui/icons/BlurOn';
 import { useEffect } from 'react';
+
+const socket = io('http://localhost:3001');
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -24,7 +27,26 @@ const useStyles = makeStyles((theme) => ({
     width: 30,
     height: 38,
     paddingLeft: 26,
-    color: '#4154b9'
+    color: '#4154b9',
+    borderRadius: 0
+  },
+  buttonSea: {
+    color: '#5e69a5',
+    minWidth: 10,
+    minHeight: 10,
+    width: 30,
+    height: 38,
+    paddingLeft: 26,
+    borderRadius: 0
+  },
+  buttonMiss: {
+    minWidth: 10,
+    minHeight: 10,
+    width: 30,
+    height: 38,
+    paddingLeft: 26,
+    color: '#939bc7',
+    borderRadius: 0
   }
 }));
 
@@ -37,35 +59,51 @@ function BattlePage(props) {
 
   const handleButtonClick = (r, c) => {
     console.log('clicked ', r, c);
+    socket.emit('shot', { row: r, column: c });
   }
 
 
 
   useEffect(() => {
-    const socket = io('http://localhost:3001');
     socket.emit('join-battle', { battleId: props.battleId.match.params.id });
     socket.on('joined', (msg) => {
       console.log('joined ', msg)
       setBoard(msg.board)
     })
+
+    socket.on('current-state', (msg) => {
+      console.log('current-state ', msg)
+      setBoard(msg.board)
+    })
+
   }, []);
-
-
 
   const boardHTML = [];
 
   for (let i = 0; i < board.length; i++) {
     for (let j = 0; j < board[0].length; j++) {
-      boardHTML.push(<Button
+
+      let iconHTML;
+      let buttonClass;
+      if (board[i][j] === 0) {
+        iconHTML = < WavesIcon />
+        buttonClass = classes.buttonSea
+      } else if (board[i][j] === -100) {
+        iconHTML = < BlurOnIcon />
+        buttonClass = classes.buttonMiss
+      }
+
+      let buttonHTML = <Button
         key={`${i}${j}`}
         onClick={() => handleButtonClick(i, j)}
         variant="contained"
         color="primary"
-        className={classes.button}
-        startIcon={<WavesIcon />}
+        className={buttonClass}
+        startIcon={iconHTML}
 
       >
-      </Button>)
+      </Button>;
+      boardHTML.push(buttonHTML)
 
       if (j === board[0].length - 1) {
         boardHTML.push(<br />)
